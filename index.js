2d3dd3f776aa312c0d934f8700db7e83404c5e33
Thawat09 <97440224+Thawat09@users.noreply.github.com>
2022-12-28 00:51:04 +0700

    65/12/27
    65/12/27

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const https = require("https")
 const express = require("express")
 const app = express()
-const PORT = process.env.PORT || 3000 //
-const TOKEN = '9UuuOoemAOcBwMH+8qg7ltt78oDQ13EXMbO6BvDkTST812/gvBvT3iaUQhrG1Jjc3DNjuQ360O2Ivp2k7n74xVrL+wjPGR3YiTa1l7mUWBScKqhZqyMY5SKX9s+Q5KPcgDxnEovactioJHpTRsHiBAdB04t89/1O/w1cDnyilFU=' // ============= เพิ่มเข้ามาใหม่
+const passport = require("passport")
+const LocalStrategy = require("passport-local")
+const PORT = process.env.PORT || 3000
+const TOKEN = '9UuuOoemAOcBwMH+8qg7ltt78oDQ13EXMbO6BvDkTST812/gvBvT3iaUQhrG1Jjc3DNjuQ360O2Ivp2k7n74xVrL+wjPGR3YiTa1l7mUWBScKqhZqyMY5SKX9s+Q5KPcgDxnEovactioJHpTRsHiBAdB04t89/1O/w1cDnyilFU='
+const User = require("./models/user")
 
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
 }))
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+passport.use(new LocalStrategy(User.authenticate()));
 
 app.get("/", (req, res) => {
     res.send('สวัสดี express webhook')
@@ -97,193 +104,205 @@ app.post("/webhook", (req, res) => {
     }
 
     else if (user_message.split(' ')[0] === "Show") {
-        // Message data, must be stringified
-        const dataString = JSON.stringify({
-            replyToken: req.body.events[0].replyToken,
-            messages: [
-                {
-                    "type": "flex",
-                    "altText": "Flex Message",
-                    "contents": {
-                        "type": "bubble",
-                        "hero": {
-                            "type": "image",
-                            "url": "https://miro.medium.com/max/1024/1*Yu0w5T7AWg8WqjVFXwaQPg.jpeg",
-                            "size": "full",
-                            "aspectRatio": "20:13",
-                            "aspectMode": "cover",
-                            "action": {
-                                "type": "uri",
-                                "uri": "http://linecorp.com/"
-                            }
-                        },
-                        "body": {
-                            "type": "box",
-                            "layout": "vertical",
-                            "contents": [
-                                {
-                                    "type": "text",
-                                    "text": "ESP8266",
-                                    "weight": "bold",
-                                    "size": "xl"
-                                },
-                                {
-                                    "type": "box",
-                                    "layout": "vertical",
-                                    "margin": "lg",
-                                    "spacing": "sm",
-                                    "contents": [
-                                        {
-                                            "type": "box",
-                                            "layout": "baseline",
-                                            "spacing": "sm",
-                                            "contents": [
-                                                {
-                                                    "type": "text",
-                                                    "text": "Details",
-                                                    "color": "#aaaaaa",
-                                                    "size": "sm",
-                                                    "flex": 1
-                                                },
-                                                {
-                                                    "type": "text",
-                                                    "text": "Khlong 6, Pathum Thani, Thailand",
-                                                    "wrap": true,
-                                                    "color": "#666666",
-                                                    "size": "sm",
-                                                    "flex": 5
-                                                }
-                                            ]
-                                        },
-                                        {
-                                            "type": "box",
-                                            "layout": "baseline",
-                                            "spacing": "sm",
-                                            "contents": [
-                                                {
-                                                    "type": "text",
-                                                    "text": "Devices",
-                                                    "color": "#aaaaaa",
-                                                    "size": "sm",
-                                                    "flex": 1
-                                                },
-                                                {
-                                                    "type": "text",
-                                                    "text": "DHT22",
-                                                    "wrap": true,
-                                                    "color": "#666666",
-                                                    "size": "sm",
-                                                    "flex": 5
-                                                }
-                                            ]
+        User.find({ 'idMicro': 'A12345' }).exec((err, doc) => {
+            var a = ""
+            for (let type of doc) {
+                a += type
+            }
+            // Message data, must be stringified
+            const dataString = JSON.stringify({
+                replyToken: req.body.events[0].replyToken,
+                messages: [
+                    {
+                        "type": "flex",
+                        "altText": "Flex Message",
+                        "contents": {
+                            "type": "bubble",
+                            "hero": {
+                                "type": "image",
+                                "url": "https://miro.medium.com/max/1024/1*Yu0w5T7AWg8WqjVFXwaQPg.jpeg",
+                                "size": "full",
+                                "aspectRatio": "20:13",
+                                "aspectMode": "cover",
+                                "action": {
+                                    "type": "uri",
+                                    "uri": "http://linecorp.com/"
+                                }
+                            },
+                            "body": {
+                                "type": "box",
+                                "layout": "vertical",
+                                "contents": [
+                                    {
+                                        "type": "text",
+                                        "text": "ESP8266",
+                                        "weight": "bold",
+                                        "size": "xl"
+                                    },
+                                    {
+                                        "type": "box",
+                                        "layout": "vertical",
+                                        "margin": "lg",
+                                        "spacing": "sm",
+                                        "contents": [
+                                            {
+                                                "type": "box",
+                                                "layout": "baseline",
+                                                "spacing": "sm",
+                                                "contents": [
+                                                    {
+                                                        "type": "text",
+                                                        "text": "Details",
+                                                        "color": "#aaaaaa",
+                                                        "size": "sm",
+                                                        "flex": 1
+                                                    },
+                                                    {
+                                                        "type": "text",
+                                                        "text": "Khlong 6, Pathum Thani, Thailand",
+                                                        "wrap": true,
+                                                        "color": "#666666",
+                                                        "size": "sm",
+                                                        "flex": 5
+                                                    }
+                                                ]
+                                            },
+                                            {
+                                                "type": "box",
+                                                "layout": "baseline",
+                                                "spacing": "sm",
+                                                "contents": [
+                                                    {
+                                                        "type": "text",
+                                                        "text": "Devices",
+                                                        "color": "#aaaaaa",
+                                                        "size": "sm",
+                                                        "flex": 1
+                                                    },
+                                                    {
+                                                        "type": "text",
+                                                        "text": a, //"DHT22"
+                                                        "wrap": true,
+                                                        "color": "#666666",
+                                                        "size": "sm",
+                                                        "flex": 5
+                                                    }
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                ]
+                            },
+                            "footer": {
+                                "type": "box",
+                                "layout": "vertical",
+                                "spacing": "sm",
+                                "contents": [
+                                    {
+                                        "type": "button",
+                                        "style": "primary",
+                                        "height": "sm",
+                                        "action": {
+                                            "type": "message",
+                                            "label": "On",
+                                            "text": "On"
                                         }
-                                    ]
-                                }
-                            ]
-                        },
-                        "footer": {
-                            "type": "box",
-                            "layout": "vertical",
-                            "spacing": "sm",
-                            "contents": [
-                                {
-                                    "type": "button",
-                                    "style": "primary",
-                                    "height": "sm",
-                                    "action": {
-                                        "type": "message",
-                                        "label": "On",
-                                        "text": "On"
+                                    },
+                                    {
+                                        "type": "button",
+                                        "style": "link",
+                                        "height": "sm",
+                                        "action": {
+                                            "type": "message",
+                                            "label": "Off",
+                                            "text": "Off"
+                                        }
+                                    },
+                                    {
+                                        "type": "box",
+                                        "layout": "vertical",
+                                        "contents": [],
+                                        "margin": "sm"
                                     }
-                                },
-                                {
-                                    "type": "button",
-                                    "style": "link",
-                                    "height": "sm",
-                                    "action": {
-                                        "type": "message",
-                                        "label": "Off",
-                                        "text": "Off"
-                                    }
-                                },
-                                {
-                                    "type": "box",
-                                    "layout": "vertical",
-                                    "contents": [],
-                                    "margin": "sm"
-                                }
-                            ],
-                            "flex": 0
+                                ],
+                                "flex": 0
+                            }
                         }
                     }
-                }
-            ]
-        })
-        // Request header
-        const headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + TOKEN
-        }
-        // Options to pass into the request
-        const webhookOptions = {
-            "hostname": "api.line.me",
-            "path": "/v2/bot/message/reply",
-            "method": "POST",
-            "headers": headers,
-            "body": dataString
-        }
-        // Define request
-        const request = https.request(webhookOptions, (res) => {
-            res.on("data", (d) => {
-                process.stdout.write(d)
+                ]
             })
-        })
-        // Handle error
-        request.on("error", (err) => {
-            console.error(err)
-        })
-        // Send data
-        request.write(dataString)
-        request.end()
+            // Request header
+            const headers = {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + TOKEN
+            }
+            // Options to pass into the request
+            const webhookOptions = {
+                "hostname": "api.line.me",
+                "path": "/v2/bot/message/reply",
+                "method": "POST",
+                "headers": headers,
+                "body": dataString
+            }
+            // Define request
+            const request = https.request(webhookOptions, (res) => {
+                res.on("data", (d) => {
+                    process.stdout.write(d)
+                })
+            })
+            // Handle error
+            request.on("error", (err) => {
+                console.error(err)
+            })
+            // Send data
+            request.write(dataString)
+            request.end()
+        });
     }
 
     else {
-        // Message data, must be stringified
-        const dataString = JSON.stringify({
-            replyToken: req.body.events[0].replyToken,
-            messages: [
-                {
-                    "type": "text",
-                    "text": "Command not found!!!"
+        User.find({ 'idMicro': 'A12345' }).exec((err, doc) => {
+            var a = ""
+            for (let type of doc) {
+                a = type
+                // Message data, must be stringified
+                const dataString = JSON.stringify({
+                    replyToken: req.body.events[0].replyToken,
+                    messages: [
+                        {
+                            "type": "text",
+                            "text": a + '\n' //"Command not found!!!"
+                        }
+                    ]
+                })
+                // Request header
+                const headers = {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + TOKEN
                 }
-            ]
-        })
-        // Request header
-        const headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + TOKEN
-        }
-        // Options to pass into the request
-        const webhookOptions = {
-            "hostname": "api.line.me",
-            "path": "/v2/bot/message/reply",
-            "method": "POST",
-            "headers": headers,
-            "body": dataString
-        }
-        // Define request
-        const request = https.request(webhookOptions, (res) => {
-            res.on("data", (d) => {
-                process.stdout.write(d)
-            })
-        })
-        // Handle error
-        request.on("error", (err) => {
-            console.error(err)
-        })
-        // Send data
-        request.write(dataString)
-        request.end()
+                // Options to pass into the request
+                const webhookOptions = {
+                    "hostname": "api.line.me",
+                    "path": "/v2/bot/message/reply",
+                    "method": "POST",
+                    "headers": headers,
+                    "body": dataString
+                }
+                // Define request
+                const request = https.request(webhookOptions, (res) => {
+                    res.on("data", (d) => {
+                        process.stdout.write(d)
+                    })
+                })
+                // Handle error
+                request.on("error", (err) => {
+                    console.error(err)
+                })
+                // Send data
+                request.write(dataString)
+                request.end()
+            }
+        });
     }
 })
 
---
+++ models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose")
+const passportLocalMongoose = require("passport-local-mongoose")
+
+const dbUrl = 'mongodb://localhost:27017/UserDB'
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).catch(err => console.log(err));
+
+let userSchema = new mongoose.Schema({
+    username: String,
+    idline: String,
+    password: String,
+    firstname: String,
+    lastname: String,
+    email: String,
+    user_id: String,
+    serialnumber: String,
+    namemicrocontroller: String,
+    useridadmin: String,
+    namesensor: String,
+    sensor: String,
+    sensorPin: String,
+    idMicro: String,
+    idSerial: String,
+    date: Date,
+    temperature: String,
+    humidity: String,
+    aqi: String,
+    current: String,
+    frequency: String,
+    onoff: Boolean,
+})
+
+userSchema.plugin(passportLocalMongoose);
+
+let User = mongoose.model('users', userSchema)
+
+module.exports = User
+
+module.exports.saveUser = ((model, data) => {
+    model.save(data)
+})
